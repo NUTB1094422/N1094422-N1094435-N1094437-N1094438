390f5d48c398defabb8ca747c4efb8646ba37001
NUTB1094422 <121710789+NUTB1094422@users.noreply.github.com>
2023-01-04 09:11:39 +0800

    Add files via upload

--- tutorial/main.py
+++ tutorial/main.py
@@ -15,6 +15,7 @@ YELLOW = (255,255,0)
 
 #遊戲初始化和創建視窗
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))  #畫面寬度高度
 pygame.display.set_caption("第一個遊戲")           #遊戲視窗上方標題
 clock = pygame.time.Clock()
@@ -28,6 +29,15 @@ rock_imgs = []
 for i in range(7):
     rock_imgs.append(pygame.image.load(os.path.join("img", f"rock{i}.png")).convert())
 
+#載入音樂
+shoot_sound = pygame.mixer.Sound(os.path.join("sound", "shoot.wav"))
+expl_sounds = [
+    pygame.mixer.Sound(os.path.join("sound", "expl0.wav")),
+    pygame.mixer.Sound(os.path.join("sound", "expl1.wav"))
+]
+pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
+pygame.mixer.music.set_volume(0.4)
+
 font_name = pygame.font.match_font('arial')
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -37,6 +47,21 @@ def draw_text(surf, text, size, x, y):
     text_rect.top = y
     surf.blit(text_surface, text_rect)
 
+def new_rock():
+    r = Rock()
+    all_sprites.add(r)
+    rocks.add(r)
+
+def draw_health(surf, hp, x, y):
+    if hp < 0:
+        hp = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (hp/100)*BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
 
 class Player(pygame.sprite.Sprite):            #飛船
     def __init__(self):
@@ -49,6 +74,7 @@ class Player(pygame.sprite.Sprite):            #飛船
         self.rect.centerx = WIDTH/2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 8                        #移動速度
+        self.health = 100
 
     def update(self): 
         key_pressed = pygame.key.get_pressed()
@@ -66,6 +92,7 @@ class Player(pygame.sprite.Sprite):            #飛船
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
+        shoot_sound.play()
 
 class Rock(pygame.sprite.Sprite):              #石頭
     def __init__(self):
@@ -125,10 +152,9 @@ bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 for i in range(8):       #8顆石頭
-    r = Rock()
-    all_sprites.add(r)
-    rocks.add(r)
+    new_rock()
 score = 0
+pygame.mixer.music.play(-1)
 
 #遊戲迴圈
 running=True
@@ -145,23 +171,25 @@ while running:
     #更新遊戲
     all_sprites.update()
     hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
-    for hit in hits:
+    for hit in hits: 
+        random.choice(expl_sounds).play()
         score += hit.radius
-        r = Rock()
-        all_sprites.add(r)
-        rocks.add(r)
+        new_rock()
 
 
-    hits = pygame.sprite.spritecollide(player, rocks, False, pygame.sprite.collide_circle)   #石頭碰撞到飛船關閉遊戲
-    if hits:
-        running = False
-
+    hits = pygame.sprite.spritecollide(player, rocks, True, pygame.sprite.collide_circle)   #石頭碰撞到飛船關閉遊戲
+    for hit in hits:
+        new_rock()
+        player.health -= hit.radius
+        if player.health <= 0:
+            running = False
 
     #畫面顯示
     screen.fill(BLACK)    #((R,G,B))：紅綠藍
     screen.blit(background_img, (0,0)) #畫上背景圖片
     all_sprites.draw(screen)
     draw_text(screen, str(score), 18, WIDTH/2, 10)
+    draw_health(screen, player.health, 5, 15)
     pygame.display.update()
 
 pygame.quit()
